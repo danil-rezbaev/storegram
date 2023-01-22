79d8c1ccf672786caff982d27f0fea7bc56d9ffe
danil-rezbaev <92051977+danil-rezbaev@users.noreply.github.com>
2023-01-22 17:58:43 +0300

    fix: Created func buttonAvailability

--- src/pages/basket/components/BasketContent.tsx
+++ src/pages/basket/components/BasketContent.tsx
@@ -1,23 +1,31 @@
-import React, { FC } from 'react'
+import React, { FC, useMemo } from 'react'
 import { useNavigate } from 'react-router-dom'
 import BasketProductList from './BasketProductList'
 import { useAppSelector } from '../../../hooks/redux'
 import BottomButton from '../../../components/BottomButton'
 import RenderPrice from '../../../components/RenderPrice'
 import PageHeader from '../../../components/page/PageHeader'
+import WayGetting from './WayGetting/WayGetting'
+import { receivingMethodData } from '../../../layout/data/basket/receivingMethodsData'
+import BasketPriceCalc from './BasketPriceCalc'
 
 export type BasketContentProps = unknown
 
 const BasketContent: FC<BasketContentProps> = () => {
   const basketStore = useAppSelector(state => state.basket)
-  const amount = basketStore.amount
-  const quantity = basketStore.quantity
+  const globalStore = useAppSelector(state => state.global)
+  const { amount, quantity } = basketStore
+  const { wayGetting } = globalStore
 
   const navigate = useNavigate()
 
   const placeOrderLink = () => {
     navigate('/place-order')
   }
+
+  const buttonAvailability = useMemo(() => {
+    return wayGetting?.filled ?? false
+  }, [wayGetting])
 
   return (
     <main>
@@ -27,9 +35,14 @@ const BasketContent: FC<BasketContentProps> = () => {
 
       <BasketProductList basketStore={basketStore} />
 
+      <WayGetting data={receivingMethodData} />
+
+      <BasketPriceCalc />
+
       <BottomButton
         title="Оформить заказ"
         onClick={placeOrderLink}
+        disabled={!buttonAvailability}
       />
     </main>
   )
