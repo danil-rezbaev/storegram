b85a59aebbf7995107feab1fbd56e982b2aa3b05
danil-rezbaev <92051977+danil-rezbaev@users.noreply.github.com>
2023-01-22 18:00:03 +0300

    fix: Removed unnecessary WayGetting component

--- src/pages/placeOrder/PlaceOrderPage.tsx
+++ src/pages/placeOrder/PlaceOrderPage.tsx
@@ -1,15 +1,11 @@
 import React from 'react'
 import Page from '../../components/page/Page'
 import PageHeader from '../../components/page/PageHeader'
-import WayGetting from '../basket/components/WayGetting/WayGetting'
-import { receivingMethodData } from '../../layout/data/basket/receivingMethodsData'
 
 function PlaceOrderPage () {
   return (
     <Page className="page--place-order">
       <PageHeader title="Оформить заказ" backLink="/basket" />
-
-      <WayGetting data={receivingMethodData} />
     </Page>
   )
 }
