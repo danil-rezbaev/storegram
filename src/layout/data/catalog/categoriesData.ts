bc59ce6b452ea0d594dbd8eb1625919727368ee2
danil-rezbaev <92051977+danil-rezbaev@users.noreply.github.com>
2023-02-15 00:30:08 +0300

    feat: Added data in categoriesData.ts

--- src/layout/data/catalog/categoriesData.ts
+++ src/layout/data/catalog/categoriesData.ts
@@ -10,5 +10,40 @@ export const categoriesData: Category[] = [
     id: 2,
     code: 'burger',
     title: 'Бургеры'
+  },
+  {
+    id: 3,
+    code: 'combo',
+    title: 'Комбо'
+  },
+  {
+    id: 4,
+    code: 'desert',
+    title: 'Десерты'
+  },
+  {
+    id: 5,
+    code: 'drinks',
+    title: 'Напитки'
+  },
+  {
+    id: 6,
+    code: 'sushi',
+    title: 'Суши'
+  },
+  {
+    id: 7,
+    code: 'rolls',
+    title: 'Роллы'
+  },
+  {
+    id: 8,
+    code: 'sets',
+    title: 'Сеты'
+  },
+  {
+    id: 9,
+    code: 'navinos',
+    title: 'Навынос'
   }
 ]
